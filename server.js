d97e3e64a7b1cff608dbb85b4fc7db0efd9796fd
Anvarjon <anvarjondevelop@gmail.com>
2025-12-22 21:58:19 +0900

    feat : created ejs file

--- server.js
+++ server.js
@@ -15,25 +15,26 @@ app.use(express.urlencoded({ extended: true })); // Agar HTML  formdan malumot y
 // 3 Views code
 //SSR — Server Side Rendering
 //SSR — HTML sahifani serverning o‘zi tayyorlab, browserga tayyor holatda yuborishi.
-app.set("view", "views"); // => EJS fayllar (front-end template) qayerda joylashganini ko'rsatadi.
+app.set("views", "views"); // => EJS fayllar (front-end template) qayerda joylashganini ko'rsatadi.
 app.set("view engine", "ejs"); // => Express’ga: “HTMLni EJS orqali generatsiya qilamiz” deb aytadi.
 
 // 4 Routing code
 
-app.get("/hello", function (req, res) {
-  //  Bu qator route (yo‘l) yaratadi.
-  res.end(`<h1 style="background: red">HELLO WORLD by David</h1>`); //foydalanuvchiga javobni yubor va aloqa tugasin
-});
+// 4 Routing code
+// app.get("/", function (req, res) {
+//   //  Bu qator route (yo‘l) yaratadi.
+//   res.end(`<h1 style="background: red">Siz asosiy oynadasiz</h1>`); //foydalanuvchiga javobni yubor va aloqa tugasin
+// });
 
-app.get("/", function (req, res) {
-  //  Bu qator route (yo‘l) yaratadi.
-  res.end(`<h1 style="background: red">Siz asosiy oynadasiz</h1>`); //foydalanuvchiga javobni yubor va aloqa tugasin
-});
+// app.get("/hello", function (req, res) {
+//   //  Bu qator route (yo‘l) yaratadi.
+//   res.end(`<h1 style="background: red">HELLO WORLD by David</h1>`); //foydalanuvchiga javobni yubor va aloqa tugasin
+// });
 
-app.get("/gift", function (req, res) {
-  // so'rov gift bo'lsa  res dagi javobni chiqar
-  res.end(`<p>Siz sovg'alar bo'limidasiz</p>`); //foydalanuvchiga javobni yubor va aloqa tugasin
-});
+// app.get("/gift", function (req, res) {
+//   // so'rov gift bo'lsa  res dagi javobni chiqar
+//   res.end(`<p>Siz sovg'alar bo'limidasiz</p>`); //foydalanuvchiga javobni yubor va aloqa tugasin
+// });
 
 const server = http.createServer(app); // “Node.js, iltimos, Express app bilan ishlaydigan server yaratib ber” degani.
 let PORT = 3000;
@@ -41,3 +42,25 @@ server.listen(PORT); //Serverni ishga tushir ! Va shu portni tinglab tur!
 //, function () {
 //   console.log(`Server ${PORT}- portda muvaffaqiyatli ishga tushdi`);
 // });
+
+// NEW CODE 19 LESSON
+app.post("/create-item", (req, res) => {
+  // ma'lumotni databasega yozish uchun
+  console.log(req.body);
+  // res.json({ test: "success" });
+  res.send("So'rov qabul qilindi");
+});
+
+app.get("/", function (req, res) {
+  // ma'lumot o'qish uchun
+  res.render("harid");
+});
+
+//19- Dars
+// public da frontedga tegishli bo'lgan js , css, html , video, foto lar turadi shu folder hammag ochiq bo'ladi
+// ejs da html code yozildi faqat uni node js da ishlataolishimimz uchun ejs fileda yozishimiz kerak
+//res.render() — Express’ning maxsus funksiyasi.
+//U EJS faylni olib, ichidagi  kodlarini JS orqali ishlab, yakuniy HTMLga aylantiradi va uni brauzerga jo‘natadi.
+
+//  * git reset --hard * => yangi yozgan kodlar kerak emas gir dagi oxirgi commitga qaytish
+// * git clean -df * =>  git repository ichidagi keraksiz, git kuzatmayotgan (untracked) fayl va papkalarni o‘chirib tashlaydi.
